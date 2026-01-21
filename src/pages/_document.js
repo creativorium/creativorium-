70999f01a467fd4406c199fd075c7e63cf32abfb
Nego <abetnego.kristiawan@gmail.com>
2026-01-21 13:07:16 +0800

    Add favicon support with updated paths to favicon folder

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,13 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           {/* meta end */}
 
+          {/* favicon begin */}
+          <link rel="icon" type="image/x-icon" href="/img/favicon/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
+          {/* favicon end */}
+
           {/* public assets begin */}
           <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css" />
           <link rel="stylesheet" href="/css/plugins/font-awesome.min.css" />
